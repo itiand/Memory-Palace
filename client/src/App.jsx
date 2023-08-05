3f1dc6d3cbd291f3cb0ff58019751a171d7d6f0b
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-08-05 15:56:39 -0400

    proxy implemented, openai quick install

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,18 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
+      <div className="collapse bg-base-200">
+        <input type="checkbox" />
+        <div className="collapse-title text-xl font-medium">
+          Click me to show/hide content
+        </div>
+        <div className="collapse-content">
+          <p>helloö222222</p>
         </div>
       </div>
     </>
