a50fe8b533e40b6fd978eb3b2ad640dfbf9a1673
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-08-05 20:12:14 -0400

    SASS install

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import ImageWithIcons from './components/ImageWithIcons';
-import './App.css';
+import './App.scss';
 
 function App() {
   
