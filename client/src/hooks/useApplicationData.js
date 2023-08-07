b53b38fcfdcd201608accc831fb18dbd60c18c96
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-08-07 15:29:44 -0400

    add placeholder data in applicationData, made photo size consistent

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -1,8 +1,34 @@
 import tailwindConfig from '../../tailwind.config.js'
 const { themes } = tailwindConfig;
 
+
+const memoryPalace = [
+  {
+    id: 1,
+    name: "palace1",
+    front_img_url: "https://images-ext-1.discordapp.net/external/rJs80p45-ElChRCSR3ELP2k_VWSEKauZphmw7PzDpfk/https/i.imgur.com/JjF0Lda.jpg?width=1036&height=1228"
+  },
+  {
+    id: 2,
+    name: "fresh prince",
+    front_img_url: "https://media.architecturaldigest.com/photos/5f60d4247cb92d03f8366538/16:9/w_2560%2Cc_limit/01_Airbnb_Fresh_Exterior-A.jpg"
+  },
+  {
+    id: 3,
+    name: "the office",
+    front_img_url: "https://i0.wp.com/lokagraph.com/wp-content/uploads/2018/05/dunder-Mifflin-building-the-office-where-location.jpg?fit=2048%2C1280"
+  },
+  {
+    id: 4,
+    name: "big bang theory",
+    front_img_url: "https://s28943.pcdn.co/wp-content/uploads/2019/06/Apt-4A-TV-Approved.jpg"
+  }
+]
+
+
 const useApplicationData = () => {
   return {
+    memoryPalace,
     themes
   }
 }
