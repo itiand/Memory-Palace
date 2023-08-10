3d4d88d03439907658b6b5e53a908db022f0a536
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-08-10 17:49:00 -0400

    expand on the Regular Palace View Modal - now reflects the selectedPalace state

--- client/src/components/RegularPalaceView.jsx
+++ client/src/components/RegularPalaceView.jsx
@@ -4,28 +4,20 @@ import { PalaceContext } from "../providers/palaceProvider";
 
 function RegularPalaceView() {
   const { selectedPalace } = useContext(PalaceContext);
+  const { PalaceName, PalaceCoverImg, Rooms } = selectedPalace;
+
+  console.log('SELECTED PALACE', selectedPalace);
   return (
     <>
       <dialog id="reg_view" className="modal">
         <form method="dialog" className="modal-box">
           <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-          <h3 className="font-bold text-lg">Palace Name</h3>
-          <img src="https://cornwall.historic-cornwall.org.uk/when_was_the_east_front_of_buckingham_palace_built.jpg" className="image-box w-70 mx-auto"></img>
-          {/* Regular Palace Modal: * Clicking Main Photo to Edit
-          Cover * Hover for Descriptions * Click Rooms to Edit
-          Rooms{" "} */}
-          <div>
-
-          </div>
-          {/* <div className="regular-modal-rooms w-full flex">
-            <img src="https://i.imgur.com/EdZmnSg.jpeg" alt="Room 1" />
-            <img src="https://i.imgur.com/rXkxaAo.jpeg" alt="Room 2" />
-            <img src="https://i.imgur.com/gNoTLLj.jpeg" alt="Room 3" />
-            <img src="https://i.imgur.com/NIYnoFP.jpeg" alt="Room 4" />
-            <img src="https://i.imgur.com/QokO0HE.jpeg" alt="Room 5" />
-          </div> */}
+          <h3 className="font-bold text-lg">{selectedPalace.PalaceName}</h3>
+          <img src={PalaceCoverImg} alt={`Cover of ${PalaceName}`} className="image-box w-70 mx-auto" />
           <div className="reg_view-rooms">
-
+            {Rooms.map((room, index) => (
+              <img key={index} src={room.img} alt={room.name} className="room-img" />
+            ))}
           </div>
         </form>
       </dialog>
@@ -33,4 +25,16 @@ function RegularPalaceView() {
   );
 }
 
+
+// {/* Regular Palace Modal: * Clicking Main Photo to Edit
+//           Cover * Hover for Descriptions * Click Rooms to Edit
+//           Rooms{" "} */}
+
+// {/* <div className="regular-modal-rooms w-full flex">
+//             <img src="https://i.imgur.com/EdZmnSg.jpeg" alt="Room 1" />
+//             <img src="https://i.imgur.com/rXkxaAo.jpeg" alt="Room 2" />
+//             <img src="https://i.imgur.com/gNoTLLj.jpeg" alt="Room 3" />
+//             <img src="https://i.imgur.com/NIYnoFP.jpeg" alt="Room 4" />
+//             <img src="https://i.imgur.com/QokO0HE.jpeg" alt="Room 5" />
+//           </div> */}
 export default RegularPalaceView;
