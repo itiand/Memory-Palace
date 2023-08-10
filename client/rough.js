89441323ad51574d58a6f58de2ded73e57652d86
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-08-09 21:41:49 -0400

    install dependencies

---
+++ client/rough.js
@@ -0,0 +1,19 @@
+//what would you like to memorize?
+  //1. 
+
+
+// ... please wait 
+
+// ... 
+//please pick or upload a palace
+  //multiple rooms
+  //this needs to get converted to a certain size
+
+
+//add new button 
+  //modal 
+    //name and desk
+
+  //photo of their choosing
+  
+  //
