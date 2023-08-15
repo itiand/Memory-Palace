a668681560c47da8e28ff7e7b0898d9f8054293e
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-08-15 19:01:50 -0400

    add house icon and fix layouts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import "./App.scss";
 
-import { useEffect, useContext, useState} from "react";
+import { useEffect, useContext, useState } from "react";
 import { themeChange } from "theme-change";
 import { PalaceContext } from "./providers/palaceProvider";
+import { BsHouseAdd } from 'react-icons/bs';
 
 // Components
 import Navbar from "./components/NavBar";
@@ -15,7 +16,7 @@ import AddMemory from "./components/AddMemory";
 
 
 const App = () => {
-  
+
   const {
     // initAndFetchNewMemoryPalace,
     //   deleteAndSwitchToLastPalace,
@@ -23,33 +24,33 @@ const App = () => {
     //   fetchMemoryPalaces,
     // setMemoryPalaces,
     themes,
-    memoryPalaces, 
-    selectedPalace, 
+    memoryPalaces,
+    selectedPalace,
     setSelectedPalace,
     selectedRoom,
     findPalaceById,
-  switchSelectPalaceById,  
-  switchToLastPalace,
-  createNewPalace,
-  deleteCurrentSelectedPalace,  
-  changePalaceEntry,
-  deletePalaceEntry, 
-  savePalaceState,
-  createPalaceExample,
-  getChatResponseFromServer,
-  getImageResponseFromServer,
-  setSelectedRoom,
-  tasks,
-} = useContext( PalaceContext );
+    switchSelectPalaceById,
+    switchToLastPalace,
+    createNewPalace,
+    deleteCurrentSelectedPalace,
+    changePalaceEntry,
+    deletePalaceEntry,
+    savePalaceState,
+    createPalaceExample,
+    getChatResponseFromServer,
+    getImageResponseFromServer,
+    setSelectedRoom,
+    tasks,
+  } = useContext(PalaceContext);
 
-const speechSynthesis = window.speechSynthesis;
-const [voices, setVoices] = useState([]);
+  const speechSynthesis = window.speechSynthesis;
+  const [voices, setVoices] = useState([]);
 
 
-useEffect(() => {
+  useEffect(() => {
     themeChange(false);
   }, []);
-  
+
   // const ToDoList = [
   //   {
   //     keyword: "Panda",
@@ -79,7 +80,7 @@ useEffect(() => {
   //     y : 2,
   //   },
   //   {
-      
+
   //     keyword: "Beaver",
   //     definition: "defintion 4",
   //     dalleImage: "",
@@ -128,191 +129,192 @@ useEffect(() => {
   //     y : 2,
   //   },
   // ];
- 
-const randomOddState = (keyword) => {
-  // takes in keyword and returns "keyword + funny string"
-  const odd = [
-    "...playing poker...",
-    "...juggling chainsaws...",
-    "...on fire...",
-    "...skydiving...",
-    "...wearing scuba gear...",
-    "...doing taxes...",
-    "...playing the xylophone...",
-    "...doing yoga...",
-    "...bungie jumping...",
-    "...surfing a wave...",
-    "...programming on a laptop...",
-    "...staging a sit-in...",
-    "...holding a lightsaber...",
-    "...riding a unicycle...", 
-    "...riding a hot air balloon...",
-    "...riding a roller-coaster...",
-    "...ice skating...", 
-    "...playing chess...",
-    "...wearing a ski-mask...",
-    "...reading a newspaper...",
-    "...eating tacos...",
-  ];
-  const randomIndex = Math.floor(Math.random() * odd.length);
-  const randomAction = odd[randomIndex];
-  return `${keyword}${randomAction}`;
-};
 
-const randomSaying = (mode) => {
-  // depending on mode, "return intro, phrase, lol string"
-  if (mode === "intro") {
-  const intro = [
-    // "...Welcome to your Memory Palace...",
-    // "...Hello There!...",
-    // "...Are you ready to begin your Memory Journey?...",
-    // "...Let's see what we can remember today!...",
-    "Welcome to your very own Mind Palace...There's a lot to learn, so let's get started!",
-  ];
-  const randomIndex = Math.floor(Math.random() * intro.length);
-  const randomIntro = `${intro[randomIndex]}...Let's begin shall we?...Your journey begins in the ${selectedRoom.roomName}...The first item on your journey is a`;
-  
-  return randomIntro;
-}
-  if (mode === "bridge") {
-    const bridge = [
-      "Next you see a",
-      "Your eyes drift towards a",
-      "You casually glance at a",
-      "Your focus and see a",
-      "Next you notice a",
-      "Suddenly, you see a",
-      "Next item on your journey is a",
-      "Next ticket on the agenda is a",
+  const randomOddState = (keyword) => {
+    // takes in keyword and returns "keyword + funny string"
+    const odd = [
+      "...playing poker...",
+      "...juggling chainsaws...",
+      "...on fire...",
+      "...skydiving...",
+      "...wearing scuba gear...",
+      "...doing taxes...",
+      "...playing the xylophone...",
+      "...doing yoga...",
+      "...bungie jumping...",
+      "...surfing a wave...",
+      "...programming on a laptop...",
+      "...staging a sit-in...",
+      "...holding a lightsaber...",
+      "...riding a unicycle...",
+      "...riding a hot air balloon...",
+      "...riding a roller-coaster...",
+      "...ice skating...",
+      "...playing chess...",
+      "...wearing a ski-mask...",
+      "...reading a newspaper...",
+      "...eating tacos...",
     ];
-    const randomIndex = Math.floor(Math.random() * bridge.length);
-    const randomPhrase = bridge[randomIndex];
-    return randomPhrase;
-  }
-  if (mode === "lol") {
-    const lol = [
-      "How odd!",
-      "Facinating!",
-      "What fun!",
-      "Golly!",
-      "Hehehehehehe!",
-      "Muahahaha!",
-      "You don't see that everyday!",
-      "Oh my!",
-      "Even I think that's hard to forget!",
-      "You're sure to remember that one!!",
-      "Buy this app!",
-      "That's adorable!",
-      "Whoever designed this, deserves an A!",
-      "Remembering isn't everything...It's the only thing!",
-      "Lock that memory in!",
-      "I wonder if I turned off my oven!",
-      "How positively drole!",
-      "All work and no play, makes Jack a dull boy!",
-      "They originally wanted Morgan Freeman for this job!",
-      "lol lol lol lol lol!",
-    ];
-    const randomIndex = Math.floor(Math.random() * lol.length);
-    const randomLol = lol[randomIndex];
-    return randomLol;
-  }
-};
-
-
-// Set up a function to read text out loud
-const speakText = (text) => {
-  const speak = () => {
-    const utterance = new SpeechSynthesisUtterance(text);
-    console.log(speechSynthesis.getVoices());
-    utterance.voice = speechSynthesis.getVoices().find(voice => voice.name === "Google UK English Female");
-    // Fred, Karen,Google UK English Male, Google UK English Female,
-    utterance.rate = 1;
-    utterance.pitch = 1;
-    speechSynthesis.speak(utterance);
+    const randomIndex = Math.floor(Math.random() * odd.length);
+    const randomAction = odd[randomIndex];
+    return `${keyword}${randomAction}`;
   };
-  return speak;
-};
 
-const generateNarrateArray = () => {
-  const ToDoList = selectedRoom.ToDoList;
-  const array = [
-    randomSaying('intro'),
-    randomOddState(ToDoList[0].keyword),
-  ];
-  // $$$ add pin interactions
-  for (let i = 1; i < ToDoList.length; i++) {array.push(
-      randomSaying('lol'),
-      randomSaying('bridge'),
-      randomOddState(`${ToDoList[i].keyword}`),
-    );
-  }
-  array.push("Well that's everything for now!...Till next time!...Bye-Bye!");
-  console.log(array);
-  return array;
-};
+  const randomSaying = (mode) => {
+    // depending on mode, "return intro, phrase, lol string"
+    if (mode === "intro") {
+      const intro = [
+        // "...Welcome to your Memory Palace...",
+        // "...Hello There!...",
+        // "...Are you ready to begin your Memory Journey?...",
+        // "...Let's see what we can remember today!...",
+        "Welcome to your very own Mind Palace...There's a lot to learn, so let's get started!",
+      ];
+      const randomIndex = Math.floor(Math.random() * intro.length);
+      const randomIntro = `${intro[randomIndex]}...Let's begin shall we?...Your journey begins in the ${selectedRoom.roomName}...The first item on your journey is a`;
 
-const startReadingAndActions = () => {
-  const fakeArray = generateNarrateArray(selectedRoom.ToDoList);
-
-  let currentIndex = 0;
-  const performAction = () => {
-    if (currentIndex < fakeArray.length) {
-      const arrayElement = fakeArray[currentIndex];
-      const speakFunction = speakText(arrayElement);
-      speakFunction();
-
-      if (arrayElement.includes('randomOddState')) {
-        const matchResult = arrayElement.match(/randomOddState\('([^']*)'\)/);
-        const keyword = matchResult ? matchResult[1] : '';
-
-        if (keyword === '') {
-          currentIndex = fakeArray.length - 1; // Skip to the last element
-        } else {
-          const oddStateResult = randomOddState(keyword);
-          // Perform actions with oddStateResult
-          console.log("Odd State Result:", oddStateResult);
-        }
-      } else if (arrayElement.includes('randomSaying')) {
-        const sayingMode = arrayElement.match(/randomSaying\('([^']+)'\)/)[1];
-        const randomSayingResult = randomSaying(sayingMode);
-        // Perform actions with randomSayingResult
-        console.log("Random Saying Result:", randomSayingResult);
-      }
-
-      currentIndex++;
-      setTimeout(performAction, 2000); // Continue after 1 second
+      return randomIntro;
+    }
+    if (mode === "bridge") {
+      const bridge = [
+        "Next you see a",
+        "Your eyes drift towards a",
+        "You casually glance at a",
+        "Your focus and see a",
+        "Next you notice a",
+        "Suddenly, you see a",
+        "Next item on your journey is a",
+        "Next ticket on the agenda is a",
+      ];
+      const randomIndex = Math.floor(Math.random() * bridge.length);
+      const randomPhrase = bridge[randomIndex];
+      return randomPhrase;
+    }
+    if (mode === "lol") {
+      const lol = [
+        "How odd!",
+        "Facinating!",
+        "What fun!",
+        "Golly!",
+        "Hehehehehehe!",
+        "Muahahaha!",
+        "You don't see that everyday!",
+        "Oh my!",
+        "Even I think that's hard to forget!",
+        "You're sure to remember that one!!",
+        "Buy this app!",
+        "That's adorable!",
+        "Whoever designed this, deserves an A!",
+        "Remembering isn't everything...It's the only thing!",
+        "Lock that memory in!",
+        "I wonder if I turned off my oven!",
+        "How positively drole!",
+        "All work and no play, makes Jack a dull boy!",
+        "They originally wanted Morgan Freeman for this job!",
+        "lol lol lol lol lol!",
+      ];
+      const randomIndex = Math.floor(Math.random() * lol.length);
+      const randomLol = lol[randomIndex];
+      return randomLol;
     }
   };
-  setTimeout(performAction, 1000); // Start after a delay of 5 seconds
-};
+
+
+  // Set up a function to read text out loud
+  const speakText = (text) => {
+    const speak = () => {
+      const utterance = new SpeechSynthesisUtterance(text);
+      console.log(speechSynthesis.getVoices());
+      utterance.voice = speechSynthesis.getVoices().find(voice => voice.name === "Google UK English Female");
+      // Fred, Karen,Google UK English Male, Google UK English Female,
+      utterance.rate = 1;
+      utterance.pitch = 1;
+      speechSynthesis.speak(utterance);
+    };
+    return speak;
+  };
+
+  const generateNarrateArray = () => {
+    const ToDoList = selectedRoom.ToDoList;
+    const array = [
+      randomSaying('intro'),
+      randomOddState(ToDoList[0].keyword),
+    ];
+    // $$$ add pin interactions
+    for (let i = 1; i < ToDoList.length; i++) {
+      array.push(
+        randomSaying('lol'),
+        randomSaying('bridge'),
+        randomOddState(`${ToDoList[i].keyword}`),
+      );
+    }
+    array.push("Well that's everything for now!...Till next time!...Bye-Bye!");
+    console.log(array);
+    return array;
+  };
+
+  const startReadingAndActions = () => {
+    const fakeArray = generateNarrateArray(selectedRoom.ToDoList);
+
+    let currentIndex = 0;
+    const performAction = () => {
+      if (currentIndex < fakeArray.length) {
+        const arrayElement = fakeArray[currentIndex];
+        const speakFunction = speakText(arrayElement);
+        speakFunction();
+
+        if (arrayElement.includes('randomOddState')) {
+          const matchResult = arrayElement.match(/randomOddState\('([^']*)'\)/);
+          const keyword = matchResult ? matchResult[1] : '';
+
+          if (keyword === '') {
+            currentIndex = fakeArray.length - 1; // Skip to the last element
+          } else {
+            const oddStateResult = randomOddState(keyword);
+            // Perform actions with oddStateResult
+            console.log("Odd State Result:", oddStateResult);
+          }
+        } else if (arrayElement.includes('randomSaying')) {
+          const sayingMode = arrayElement.match(/randomSaying\('([^']+)'\)/)[1];
+          const randomSayingResult = randomSaying(sayingMode);
+          // Perform actions with randomSayingResult
+          console.log("Random Saying Result:", randomSayingResult);
+        }
+
+        currentIndex++;
+        setTimeout(performAction, 2000); // Continue after 1 second
+      }
+    };
+    setTimeout(performAction, 1000); // Start after a delay of 5 seconds
+  };
 
 
 
-const handlePalaceClick = (selected) => {
-  setSelectedPalace(selected); // clicked palace as the selected palace state
-  window.reg_view.showModal();
-};
+  const handlePalaceClick = (selected) => {
+    setSelectedPalace(selected); // clicked palace as the selected palace state
+    window.reg_view.showModal();
+  };
 
-const memoryPalaceCarousel = memoryPalaces.map((palace) => {
+  const memoryPalaceCarousel = memoryPalaces.map((palace) => {
+    return (
+      <PalaceCarouselItem
+        key={palace._id}
+        palace={palace}
+        onPalaceClick={handlePalaceClick}
+      />
+    );
+  });
+
+  // const handleTestClick1 = () => {
+  // };
+  const handleTestClick2 = () => {
+  };
+  const handleTestClick3 = () => {
+  };
+
+
   return (
-    <PalaceCarouselItem
-      key={palace._id}
-      palace={palace}
-      onPalaceClick={handlePalaceClick}
-    />
-  );
-});
-
-// const handleTestClick1 = () => {
-// };
-const handleTestClick2 = () => {
-};
-const handleTestClick3 = () => {
-};
-
-
-return (
-  <>
+    <>
       <Navbar themes={themes} />
       <RegularPalaceView />
       <AddNewPalace />
@@ -320,21 +322,18 @@ return (
       <AddRoom />
       <AddMemory />
       <div className="-mt-8 flex flex-col items-center justify-around h-5/6">
-        <h1 className="mt-6 text-center text-5xl">My Palaces</h1>
-        <div className="container  text-center carousel-container mx-auto">
-          <div className="carousel mx-auto mt-7 rounded-lg">
-            {memoryPalaceCarousel}
+        <div>
+          <h1 className="mt-6 text-center text-5xl">My Palaces</h1>
+          <div className="container  text-center carousel-container mx-auto">
+            <div className="carousel mx-auto mt-7 rounded-lg">
+              {memoryPalaceCarousel}
+            </div>
           </div>
         </div>
-        <br></br>
-        <div>
-          <h2>Tester Buttons</h2>
-          <button className="btn" onClick={startReadingAndActions}>Click 1</button>
-          <button className="btn" onClick={handleTestClick2}>Click 2</button>
-          <button className="btn" onClick={handleTestClick3}>Click 3</button>
+        <div className="text-gray-400 hover:text-gray-700 text-4xl cursor-pointer add-palace" onClick={() => window.add_palace_view.showModal()}>
+          <BsHouseAdd></BsHouseAdd>
         </div>
-        <img src={selectedPalace.NewImage}></img>
-
+        <br></br>
       </div>
     </>
   );
