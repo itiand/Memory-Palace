1b09cfe9429ae8118ad994986609463397ac12d4
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-08-16 19:12:05 -0400

    add logo

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,6 +2,8 @@
 // import AddNewPalace from "./AddNewPalace";
 import PropTypes from "prop-types";
 import { BsHouseAdd } from 'react-icons/bs';
+import postgresLogoURL from '../assets/postgres.svg';
+
 
 
 function Navbar(props) {
@@ -29,47 +31,10 @@ function Navbar(props) {
             </option>
           ))}
         </select>
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
-        <a className="btn btn-ghost normal-case text-xl">ELIFINT</a>
+        <a className="btn btn-ghost normal-case text-2xl flex-start">
+          <img src={postgresLogoURL} alt="Postgres Logo" className="h-12 w-12" />
+          ELIFINT
+        </a>
       </div>
 
       {/* <div className="navbar-center hidden lg:flex">
