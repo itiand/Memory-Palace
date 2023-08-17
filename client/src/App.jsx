36f10b3bf71e5c98461ea092646f60b4bd700207
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-08-17 00:33:56 -0400

    add second carousel for favorites section:

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { useEffect, useContext, useState } from "react";
 import { themeChange } from "theme-change";
 import { PalaceContext } from "./providers/palaceProvider";
 import { BsHouseAdd } from 'react-icons/bs';
+import { FaRegEye, FaEdit, FaPlus, FaCheck, FaTimes } from 'react-icons/fa';
 
 
 // Components
@@ -40,7 +41,7 @@ const App = () => {
     tasks,
   } = useContext(PalaceContext);
 
-  
+
   useEffect(() => {
     themeChange(false);
   }, []);
@@ -62,14 +63,14 @@ const App = () => {
 
   return (
     <>
-    
+
       <Navbar themes={themes} />
       <RegularPalaceView />
       <AddNewPalace />
       <AddPalaceImage />
       <AddRoom />
       <AddMemory />
-      <div className="-mt-8 flex flex-col items-center justify-around h-5/6">
+      <div className="-mt-8 flex flex-col items-center justify-start">
         <div>
           <h1 className="mt-6 text-center text-neutral text-5xl">Palace Collection</h1>
           <div className="container  text-center carousel-container mx-auto">
@@ -78,10 +79,30 @@ const App = () => {
             </div>
           </div>
         </div>
-        <div className=" text-gray-400 bg-base-200 py-3 px-5 rounded-xl hover:bg-base-300 cursor-pointer add-palace" onClick={() => window.add_palace_view.showModal()}>
-          <BsHouseAdd></BsHouseAdd>
+      </div>
+      <div className="grid grid-cols-4 gap-4 w-4/6 m-auto">
+        <div className="flex items-center justify-center">
+          <div className="text-gray-400 bg-base-200 py-3 px-5 rounded-xl hover:bg-base-300 cursor-pointer add-palace" onClick={() => window.add_palace_view.showModal()}>
+            <BsHouseAdd></BsHouseAdd>
+          </div>
         </div>
-        <br></br>
+        <div className="col-span-3">
+          <h1 className="mt-6 text-center text-neutral text-3xl">Favorites</h1>
+
+          <div className="carousel rounded-box w-full gap-x-1 cursor-pointer max-h-80">
+            {memoryPalaces.map((palace, index) => {
+              return (
+                <div key={index} className="carousel-item w-1/2 relative">
+                  <img src={palace.PalaceCoverImg} className="w-full" />
+                  <div className="overlay absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center opacity-0 hover:opacity-60 bg-black">
+                    <span className="mb-1 text-white text-2xl">{palace.PalaceName}</span>
+                    <span className="text-lg py-1 px-2 rounded text-white hover:text-2xl hover:ease-in-out duration-200" onClick={() => window.add_palace_view.showModal()}><FaRegEye /></span>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </>
   );
