ae4cdef94b1ea2f2f1c52863c0b81f037f42b65b
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-08-21 17:17:10 -0400

    readjust home padding and margins

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -65,49 +65,51 @@ const App = () => {
       <AddPalaceImage />
       <AddRoom />
       <AddMemory />
-      <div className="container m-auto mb-4 ">
-        <div className="carousel-container container mx-auto text-center">
-          <h1 className="mt-6 text-left text-lg text-gray-600">
-            Palace Collection
-          </h1>
-          <div className="carousel mx-auto rounded-lg">
-            {memoryPalaceCarousel}
+      <div className="body -mt-6 pt-6">
+        <div className="container m-auto mb-4 ">
+          <div className="carousel-container container mx-auto text-center">
+            <h1 className="mt-6 text-left text-lg text-gray-600">
+              Palace Collection
+            </h1>
+            <div className="carousel mx-auto rounded-lg">
+              {memoryPalaceCarousel}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="container m-auto grid w-4/6 grid-cols-4 gap-4">
-        <div className="col-span-3">
-          <h1 className="mt-6 text-left text-lg text-gray-600">
-            Favorite Palaces
-          </h1>
+        <div className="container m-auto grid w-4/6 grid-cols-4 gap-4">
+          <div className="col-span-3">
+            <h1 className=" text-left text-lg text-gray-600">
+              Favorite Palaces
+            </h1>
 
-          <div className="carousel rounded-box max-h-80 w-full cursor-pointer gap-x-1">
-            {memoryPalaces.map((palace, index) => {
-              return (
-                <div key={index} className="carousel-item relative w-1/2">
-                  <img src={palace.PalaceCoverImg} className="w-full" />
-                  <div className="overlay absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center bg-black opacity-0 hover:opacity-60">
-                    <span className="mb-1 text-2xl text-white">
-                      {palace.PalaceName}
-                    </span>
-                    <span
-                      className="rounded px-2 py-1 text-lg text-white duration-200 hover:text-2xl hover:ease-in-out"
-                      onClick={() => handlePalaceClick(palace)}
-                    >
-                      <FaRegEye />
-                    </span>
+            <div className="carousel rounded-box max-h-80 w-full cursor-pointer gap-x-1">
+              {memoryPalaces.map((palace, index) => {
+                return (
+                  <div key={index} className="carousel-item relative w-1/2">
+                    <img src={palace.PalaceCoverImg} className="w-full" />
+                    <div className="overlay absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center bg-black opacity-0 hover:opacity-60">
+                      <span className="mb-1 text-2xl text-white">
+                        {palace.PalaceName}
+                      </span>
+                      <span
+                        className="rounded px-2 py-1 text-lg text-white duration-200 hover:text-2xl hover:ease-in-out"
+                        onClick={() => handlePalaceClick(palace)}
+                      >
+                        <FaRegEye />
+                      </span>
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
-        </div>
-        <div className="flex items-center justify-center">
-          <div
-            className="add-palace cursor-pointer rounded-xl bg-indigo-500 px-5 py-3 text-white hover:bg-indigo-600"
-            onClick={() => window.add_palace_view.showModal()}
-          >
-            <BsHouseAdd></BsHouseAdd>
+          <div className="flex items-center justify-center">
+            <div
+              className="add-palace cursor-pointer rounded-xl bg-indigo-500 px-5 py-3 text-white hover:bg-indigo-600"
+              onClick={() => window.add_palace_view.showModal()}
+            >
+              <BsHouseAdd></BsHouseAdd>
+            </div>
           </div>
         </div>
       </div>
